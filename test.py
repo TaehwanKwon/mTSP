0b2744d232f9990eb774951b003957cfb8531b62
권태환 <taehwan@gwontaehwan-ui-MacBook-Air-5.local>
2021-05-22 20:36:34 +0900

    testing training

--- test.py
+++ test.py
@@ -2,20 +2,26 @@ from pprint import pprint
 from IPython import embed
 
 import logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.ERROR)
+logger = logging.getLogger(__name__)
 
 from envs.mtsp_simple import MTSP, MTSPSimple
 from models.gnn import Model
+from agent.QLearning import Agent
+from utils.simulator import Simulator
 
-config = __import__('conf.conf-kth', fromlist=[None]).config
+config = __import__('conf.conf-test', fromlist=[None]).config
 
-model = Model(config)
+model = Model(config=config)
+model.initialize_batch()
+agent = Agent(config=config)
+simulator = Simulator(config=config, model=model)
 
-mtsp = MTSP(config)
-s = mtsp.reset()
+simulator.save_to_replay_buffer(config['learning']['size_replay_buffer'])
+for step_train in range(100):
+    model.step_train = step_train
+    processed_batch = model.get_processed_batch()
+    loss = Agent.get_loss(processed_batch)
+    print(f"loss: {loss}")
 
-for _ in range(10):
-    action = model.action(s)
-    s, r, done = mtsp.step(action)
-    mtsp.render()
-    if done: break
+    
