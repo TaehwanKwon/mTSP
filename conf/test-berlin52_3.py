b7c13875bd5ee616e7649b4759cd9511ca1e378e
Park, Yongchan <yc38@ncsoft.com>
2021-06-04 14:55:05 +0900

    added configs for test

---
+++ conf/test-berlin52_3.py
@@ -0,0 +1,34 @@
+import os
+import sys
+sys.path.append(os.path.abspath( os.path.join(os.path.dirname(__file__), "..")))
+
+config = {
+    'env':{
+        'name':'MTSP',
+        'num_robots': 3,
+        'num_cities': 51,
+        'file': 'berlin52.txt',
+        'scale_distance': 1e-3,
+        'scale_reward':2.5e-4,
+    },
+    'learning':{
+        'step': 400000,
+        'presence_prev':False,
+        'algorithm': 'optimal_q_learning',
+        #'algorithm': 'sarsa',
+        'lr_start': 5e-4,
+        'lr_end': 1e-4,
+        'lr_step': 500,
+        'lr_decay': 0.99, 
+        'eps': { # eps = eps_end + eps_add * half_life / (half_life + training_step)
+            'add': 0.9,
+            'end': 0.1,
+            'half_life': 10000,
+            },
+        'gamma': 1.0,
+        'size_batch': 64,
+        'size_replay_buffer': 10000,
+        'num_rollout':1,
+        'num_processes': 2,
+    }
+}
---
+++ conf/test-rat99_2.py
@@ -0,0 +1,34 @@
+import os
+import sys
+sys.path.append(os.path.abspath( os.path.join(os.path.dirname(__file__), "..")))
+
+config = {
+    'env':{
+        'name':'MTSP',
+        'num_robots': 2,
+        'num_cities': 98,
+        'file': 'rat99.txt',
+        'scale_distance': 0.00737,
+        'scale_reward':2.5e-4,
+    },
+    'learning':{
+        'step': 400000,
+        'presence_prev':False,
+        'algorithm': 'optimal_q_learning',
+        #'algorithm': 'sarsa',
+        'lr_start': 5e-4,
+        'lr_end': 1e-4,
+        'lr_step': 500,
+        'lr_decay': 0.99, 
+        'eps': { # eps = eps_end + eps_add * half_life / (half_life + training_step)
+            'add': 0.9,
+            'end': 0.1,
+            'half_life': 10000,
+            },
+        'gamma': 1.0,
+        'size_batch': 64,
+        'size_replay_buffer': 10000,
+        'num_rollout':1,
+        'num_processes': 2,
+    }
+}
